900dd0fa0caf8524b53564e663b4c582a4ca914b
kayleeha <ampmgrow.go@gmail.com>
2022-11-22 15:21:02 +0900

    commit message

---
+++ src/main/java/com/cos/blog/test/HttpControllerTest.java
@@ -0,0 +1,31 @@
+package com.cos.blog.test;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HttpControllerTest {
+
+	@GetMapping("/http/get")
+	public String getTest() {
+		return "get 요청";
+	}
+	
+	@PostMapping("/http/post")
+	public String postTest() {
+		return  "post 요청";
+	}
+	
+	@PutMapping("/http/put")
+	public String putTest() {
+		return  "put 요청";
+	}
+	
+	@DeleteMapping("/http/delete")
+	public String deleteTest() {
+		return  "delete 요청";
+	}
+}
